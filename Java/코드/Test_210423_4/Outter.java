f32b9d4ccd62e2f6f9ff119ea218ac9bfaf459d6
nayeon <nayeonvita@naver.com>
2021-04-24 21:48:19 +0900

    [Add] 중첩클래스210423_4

---
+++ Java/코드/Test_210423_4/Outter.java
@@ -0,0 +1,25 @@
+package Test_210423_4;
+
+public class Outter {
+	
+	public void method1(final int arg) {
+		final int localVariable = 1;
+		
+		class Inner{
+			public void method() {
+				int result = arg + localVariable;
+			}
+		}
+	}
+	
+	public void method2(int arg) {
+		int localVariable = 1;
+		
+		class Inner {
+			public void method() {
+				int result = arg + localVariable;
+			}
+		}
+	}
+
+}
